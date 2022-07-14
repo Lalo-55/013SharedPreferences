82b2560e7cd0fd7421f5984af9f4a25454de6367
Edu-Lu <moralesluis17106494@gmail.com>
2022-07-14 09:42:10 -0500

    Ejercicios resueltos

--- app/src/main/java/com/luismorales17106494/a013almacenamientodedatosmediantelaclasesharedpreferences/ActivityProblema2.kt
+++ app/src/main/java/com/luismorales17106494/a013almacenamientodedatosmediantelaclasesharedpreferences/ActivityProblema2.kt
@@ -1,11 +1,46 @@
 package com.luismorales17106494.a013almacenamientodedatosmediantelaclasesharedpreferences
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.luismorales17106494.a013almacenamientodedatosmediantelaclasesharedpreferences.databinding.ActivityProblema1Binding
+import com.luismorales17106494.a013almacenamientodedatosmediantelaclasesharedpreferences.databinding.ActivityProblema2Binding
 
 class ActivityProblema2 : AppCompatActivity() {
+    private lateinit var _binding: ActivityProblema2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_problema2)
+        _binding = ActivityProblema2Binding.inflate(layoutInflater)
+        setContentView(_binding.root)
+
+        val store = this.getSharedPreferences("dtb", 0)
+        var SHARED_USER: String
+
+
+        _binding.btGrabar.setOnClickListener() {
+            if (_binding.etName.text.isNotBlank() && _binding.etDatos.text.isNotBlank()) {
+                SHARED_USER = _binding.etName.text.toString()
+
+                store.edit().putString(SHARED_USER, _binding.etDatos.text.toString()).apply()
+                Toast.makeText(
+                    this, "Correo guardado correctamente",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            else{
+                Toast.makeText(
+                    this, "ingrese los datos",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            }
+        }
+        _binding.btMostrar.setOnClickListener(){
+            val datos = store.getString(_binding.etName.text.toString(), "registro no encontrado")
+            _binding.etDatos.setText(datos)
+        }
+
+
     }
 }
--- app/src/main/java/com/luismorales17106494/a013almacenamientodedatosmediantelaclasesharedpreferences/MainActivity.kt
+++ app/src/main/java/com/luismorales17106494/a013almacenamientodedatosmediantelaclasesharedpreferences/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.luismorales17106494.a013almacenamientodedatosmediantelaclasesharedpreferences
 
+import android.app.Activity
+import android.app.ActivityManager
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -39,6 +41,16 @@ Problema 2:
 Confeccionar un programa que permita administrar una agenda personal.
 Nuestra clave será el nombre de la persona.
  */
+
+/*
+Problema propuesto
+
+Realizar un programa que genere un número aleatorio entre 1 y 50, pedir que el operador lo adivine,
+informar si ganó o si el número es mayor o menor al ingresado. Cuando el operador lo adivine
+incrementar en uno el puntaje de juego. Cada vez que se ingrese a la aplicación mostrar el
+puntaje actual, es decir recordar el puntaje en un archivo de preferencias.
+
+ */
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,7 +58,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnProblema1.setOnClickListener(){
+        binding.btnProblema1.setOnClickListener() {
             val myIntent = Intent(
                 this,
                 ActivityProblema1::class.java
@@ -54,7 +66,20 @@ class MainActivity : AppCompatActivity() {
             startActivity(myIntent)
         }
 
+        binding.btnProblema2.setOnClickListener() {
+            val myIntet = Intent(
+                this,
+                ActivityProblema2::class.java
+            )
+            startActivity(myIntet)
+        }
+        binding.btnProblema3.setOnClickListener() {
+            startActivity(Intent(this,ActivityProblemaPropuesto::class.java))
+
+        }
 
 
     }
+
+
 }
